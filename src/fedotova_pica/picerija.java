f509d5f5b5d20590e230e9e279715be286f751f1
Gerda <gerda.fedotova.sk@lvt.lv>
2024-04-23 15:11:54 +0300

    Izveidoju izveles

--- src/fedotova_pica/picerija.java
+++ src/fedotova_pica/picerija.java
@@ -1,10 +1,18 @@
 package fedotova_pica;
 
+import javax.swing.JOptionPane;
+
 public class picerija {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+			String[] darbibas = {"Jauns pircejs", "Aktivie pasutijumi", "Pabeigti pasutijumi", "Apturet"};
+			String izvele;
+			
+			do {
+				izvele = (String)JOptionPane.showInputDialog(null, "Darbibas", "Pica", JOptionPane.QUESTION_MESSAGE, null, darbibas, darbibas[0]);
+				
+			}while(!izvele.equals("Apturet"));
 	}
 
 }
